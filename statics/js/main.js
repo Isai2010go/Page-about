f3a2accdc9ff0747c557384a1b15d8095b194a5b
Isai2010go <isai2010go@gmail.com>
2026-01-02 14:29:32 +0800

    Update API URL for fetching posts

--- statics/js/main.js
+++ statics/js/main.js
@@ -18,7 +18,7 @@ function getAchives(){
     t = ``;
     $.ajax({
         type:"GET",
-        url:"https://www.spacezero.top/api/get_posts/",
+        url:"https://www.spacezero.top/wp-json/wp/v2/posts?per_page=8&page=1",
         dataType:"json",
         success:function(json){
             for(var i = 0;i < json.length;i++){
